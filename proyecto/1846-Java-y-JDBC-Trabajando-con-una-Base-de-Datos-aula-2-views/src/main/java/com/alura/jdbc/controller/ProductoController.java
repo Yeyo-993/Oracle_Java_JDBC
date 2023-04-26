5d691e6cce39e11b470846fc7ad1b3b4952d4565
Sergio Reinoso <slrs993@gmail.com>
2023-04-25 22:55:08 -0500

    Listado de statement #2 completado

--- proyecto/1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ proyecto/1846-Java-y-JDBC-Trabajando-con-una-Base-de-Datos-aula-2-views/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -14,13 +14,15 @@ public class ProductoController {
 
 	public void modificar(String nombre, String descripcion, Integer id) {
 		// TODO
+		
 	}
 
 	public void eliminar(Integer id) {
 		// TODO
+		
 	}
 
-	public List<?> listar() throws SQLException {
+	public List<Map<String, String>> listar() throws SQLException {
 		
 		Connection con = DriverManager.getConnection(
 				"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
@@ -36,6 +38,7 @@ public class ProductoController {
 		List<Map<String, String>> resultado = new ArrayList<>();
 		
 		while (resultSet.next()) {
+			
 			Map<String, String> fila = new HashMap<>();
 			fila.put("ID", String.valueOf(resultSet.getInt("ID")));
 			fila.put("NOMBRE", resultSet.getString("NOMBRE"));
@@ -43,15 +46,18 @@ public class ProductoController {
 			fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
 			
 			resultado.add(fila);
+			
 		}
 		
 		con.close();
 		
 		return resultado;
+		
 	}
 
     public void guardar(Object producto) {
 		// TODO
+    	
 	}
 
 }
